3601bbc95a75b802288f6b105ed037b1db0b2760
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-27 20:35:30 +0200

    feat: add MainLayout

--- bool-shop-react/src/App.jsx
+++ bool-shop-react/src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import SearchPage from "./pages/SearchPage"
+import MainLayout from "./layouts/MainLayout"
 
 function App() {
 
   return (
     <>
-
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="*" element={<div>page not found</div>} />
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="*" element={<div>page not found</div>} />
+          </Route>
         </Routes>
-      </BrowserRouter >
+      </BrowserRouter>
     </>
   )
 }
