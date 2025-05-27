7eee93a31ce737f03ca063dc9e6b827ad6f2e5bc
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-27 20:34:56 +0200

    feat: create component Header

---
+++ bool-shop-react/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom"
+
+function Header() {
+
+    return <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                    <NavLink className="navbar-brand" to='/'>
+                        <img src="/public/bool-shop-logo.png" alt="Logo" width="70" height="70" />
+                    </NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page" to='/'>Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to='/search'>Search Product</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </>
+}
+
+export default Header
