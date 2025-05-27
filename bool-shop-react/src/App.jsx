ad63802b8ee68711c96d8292ef940d14fe018e64
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-27 15:56:20 +0200

    feat: add bootstrap

--- bool-shop-react/src/App.jsx
+++ bool-shop-react/src/App.jsx
@@ -2,8 +2,14 @@ function App() {
 
   return (
     <>
-      <h1>hello word</h1>
-      <h2>Ci proviamo</h2>
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
     </>
   )
 }
