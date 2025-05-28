e83ef68edb1905ca31b6b43d3d426f5bb4c54393
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-28 12:26:50 +0200

    feat: add new path for TestPage

--- bool-shop-react/src/App.jsx
+++ bool-shop-react/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import SearchPage from "./pages/SearchPage"
 import MainLayout from "./layouts/MainLayout"
+import TestPage from "./pages/TestPage"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/search" element={<SearchPage />} />
+            <Route path="/test" element={<TestPage />} />
             <Route path="*" element={<div>page not found</div>} />
           </Route>
         </Routes>
