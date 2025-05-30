843189ffdbd08c78adb7bf59fee1b1c047dc0289
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-30 20:44:06 +0200

    feat: add class-style to main to set viewport height to 100vh, to set footer always down in layout

--- bool-shop-react/src/layouts/MainLayout.jsx
+++ bool-shop-react/src/layouts/MainLayout.jsx
@@ -9,7 +9,7 @@ function MainLayout() {
     return <>
         <Header />
         <Menu />
-        <main className="container">
+        <main className="container flex-grow-1">
             <Outlet />
         </main>
         <Footer />
