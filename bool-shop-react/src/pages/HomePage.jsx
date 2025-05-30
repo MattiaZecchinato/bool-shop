b820bd0a2f583a7656169624f96524653989e086
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-30 16:55:17 +0200

    feat: add hover cart

--- bool-shop-react/src/pages/HomePage.jsx
+++ bool-shop-react/src/pages/HomePage.jsx
@@ -74,9 +74,9 @@ function HomePage() {
 
             </div>
 
-            <div className="cart-box">
+            {/* <div className="cart-box">
                 <SideCart />
-            </div>
+            </div> */}
         </div>)
 }
 
