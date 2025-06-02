79fbed2fbcd86736801546eb0ab6286a4ce3a776
giorgiameffe <giorgiameffe03@gmail.com>
2025-06-02 11:07:33 +0200

    feat: modify font size

--- bool-shop-react/src/components/Footer.jsx
+++ bool-shop-react/src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
             <div className="container">
                 <div className="row d-flex justify-content-around align-items-center">
                     <div className="col-md-3 col-12 text-center">
-                        <h4 className="fw-bold">Link Utili</h4>
+                        <h5 className="fw-bold">Link Utili</h5>
                         <ul className="list-unstyled">
                             <li><NavLink className="text-decoration-none text-white" to="/">Home</NavLink></li>
                             <li><NavLink className="text-decoration-none text-white" to="/search">Catalogo</NavLink></li>
@@ -22,7 +22,7 @@ function Footer() {
                         <figure>
                             <img src="../public/bool-shop-logo.png" alt="logo" className="w-25" />
                         </figure>
-                        <h2 className="mb-3 fw-bold">Unisciti al mondo dei giochi da tavolo!</h2>
+                        <h4 className="mb-3 fw-bold">Unisciti al mondo dei giochi da tavolo!</h4>
                         <p className="fst-italic">Resta aggiornato su nuovi arrivi, offerte speciali e consigli di gioco.</p>
                         <ul className="list-unstyled">
                             <li>Via del Gioco, 12 – 00100 Roma</li>
