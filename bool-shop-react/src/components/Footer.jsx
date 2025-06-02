275ecb7d17e6d00bfbf19f5724c734360a074da0
giorgiameffe <giorgiameffe03@gmail.com>
2025-06-02 10:56:36 +0200

    feat: fix footer

--- bool-shop-react/src/components/Footer.jsx
+++ bool-shop-react/src/components/Footer.jsx
@@ -5,11 +5,11 @@ import { NavLink } from "react-router-dom";
 function Footer() {
 
     return (
-        <footer className="bg-dark text-light p-5 mt-5">
+        <footer className="text-light p-5 mt-5">
             <div className="container">
                 <div className="row d-flex justify-content-around align-items-center">
                     <div className="col-md-3 col-12 text-center">
-                        <h4>Link Utili</h4>
+                        <h4 className="fw-bold">Link Utili</h4>
                         <ul className="list-unstyled">
                             <li><NavLink className="text-decoration-none text-white" to="/">Home</NavLink></li>
                             <li><NavLink className="text-decoration-none text-white" to="/search">Catalogo</NavLink></li>
@@ -22,7 +22,7 @@ function Footer() {
                         <figure>
                             <img src="../public/bool-shop-logo.png" alt="logo" className="w-25" />
                         </figure>
-                        <h2 className="mb-3">Unisciti al mondo dei giochi da tavolo!</h2>
+                        <h2 className="mb-3 fw-bold">Unisciti al mondo dei giochi da tavolo!</h2>
                         <p className="fst-italic">Resta aggiornato su nuovi arrivi, offerte speciali e consigli di gioco.</p>
                         <ul className="list-unstyled">
                             <li>Via del Gioco, 12 – 00100 Roma</li>
@@ -31,11 +31,11 @@ function Footer() {
                         </ul>
                     </div>
                     <div className="col-md-3 text-md-end text-center col-12">
-                        <h5 className="text-center pb-2">Seguici sui social</h5>
+                        <h5 className="text-center pb-2 fw-bold">Seguici sui social</h5>
                         <ul className="text-center list-unstyled d-flex justify-content-center gap-3">
                             <li>
-                                <a href="https://www.facebook.com/">
-                                    <FontAwesomeIcon icon={faSquareFacebook} size="2x" color="#1877F2" />
+                                <a href="https://x.com/">
+                                    <FontAwesomeIcon icon={faSquareXTwitter} size="2x" color="#000" />
                                 </a>
                             </li>
                             <li>
@@ -44,8 +44,8 @@ function Footer() {
                                 </a>
                             </li>
                             <li>
-                                <a href="https://x.com/">
-                                    <FontAwesomeIcon icon={faSquareXTwitter} size="2x" color="#000" />
+                                <a href="https://www.facebook.com/">
+                                    <FontAwesomeIcon icon={faSquareFacebook} size="2x" color="#1877F2" />
                                 </a>
                             </li>
                         </ul>
