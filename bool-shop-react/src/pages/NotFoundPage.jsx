7149f00db61ab2214f73fd62b318675a72a80ca1
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-06-03 21:11:03 +0200

    feat: add style to not-found page

--- bool-shop-react/src/pages/NotFoundPage.jsx
+++ bool-shop-react/src/pages/NotFoundPage.jsx
@@ -1,15 +1,17 @@
 import { NavLink, useNavigate } from "react-router-dom"
+import wizard from "../assets/wizard.png"
 
 function NotFoundPage() {
 
     const navigate = useNavigate()
 
     return <>
-        <h1 className="text-center text-white mb-4">Houston, abbiamo un problema: 404.</h1>
-        <div className="container d-flex justify-content-center mb-3">
-            <img src="./src/assets/not-found.gif" alt="not found" />
+        <h1 className="text-center text-white my-5">Ops, si è verificato un problema: 404!</h1>
+        <div className="container d-flex justify-content-center mb-5">
+            <img className="not-found-img" src={wizard} alt="not-found" />
         </div>
-        <div className="text-center">
+        <div className="text-white text-center mb-5">
+            <p className="fs-5">A quanto pare la pagina che stavi cercando deve essere stata rubata da un goblin, ti consiglio di tornare sulla retta via!</p>
             <button className="btn btn-light" onClick={() => navigate('/')}>Torna alla home</button>
         </div>
     </>
