a9e20024562cae184fbd3fc843abe270a82ad993
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-06-03 12:40:05 +0200

    feat: create OrderRecapPage

---
+++ bool-shop-react/src/pages/OrederRecap.jsx
@@ -0,0 +1,24 @@
+import { useLocation } from "react-router-dom"
+
+function OrderRecap() {
+
+    const location = useLocation()
+    const dataRecap = location.state?.payload
+    console.log(dataRecap)
+
+    const { user_first_name, user_email, total_order } = dataRecap
+
+    return <>
+        <div className="text-white">
+            <h1>🎉 Ordine completato con successo!</h1>
+            <p>Grazie, <strong>{user_first_name}</strong>, per il tuo acquisto.</p>
+            <p>Una conferma è stata inviata a <strong>{user_email}</strong>.</p>
+            <p>Numero ordine: <strong>#</strong></p> {/* questo e da vedere se si implementa */}
+            <p>Totale: <strong>€{total_order}</strong></p>
+
+            <p>Riceverai una notifica quando il tuo ordine sarà spedito.</p>
+        </div>
+    </>
+}
+
+export default OrderRecap
