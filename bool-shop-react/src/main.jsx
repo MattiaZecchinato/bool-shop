9f83b5007164400d39ebc20d1d12884ca550af1f
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-06-03 09:17:30 +0200

    fix: change style

--- bool-shop-react/src/main.jsx
+++ bool-shop-react/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App'
 
 //css bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 //js bootstrap
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+//css index
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
