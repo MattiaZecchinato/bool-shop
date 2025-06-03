f18d0cbf16ca9f3ac3ecf1b6df10dcc2d5b10918
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-06-03 20:52:54 +0200

    feat: add style to recap-page

--- bool-shop-react/src/pages/OrederRecap.jsx
+++ bool-shop-react/src/pages/OrederRecap.jsx
@@ -1,4 +1,6 @@
-import { useLocation, useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom";
+import magic from "../assets/magic.png";
+import staff from "../assets/staff.png";
 
 function OrderRecap() {
 
@@ -10,16 +12,16 @@ function OrderRecap() {
     const { user_first_name, user_email, total_order } = dataRecap
 
     return <>
-        <div className="text-white">
-            <h1>🎉 Ordine completato con successo!</h1>
+        <div className="text-white card-detail-width recap-container my-5">
+            <h1 className="mb-4"><img className="image-recap me-2" src={staff} alt="staff" /><img className="image-recap position-image-recap" src={magic} alt="magic" />Ordine completato con successo!</h1>
             <p>Grazie, <strong>{user_first_name}</strong>, per il tuo acquisto.</p>
             <p>Una conferma è stata inviata a <strong>{user_email}</strong>.</p>
             <p>Numero ordine: <strong>#</strong></p> {/* questo e da vedere se si implementa */}
             <p>Totale: <strong>€{total_order}</strong></p>
 
-            <p>Riceverai una notifica quando il tuo ordine sarà spedito.</p>
+            <p className="mb-5">Riceverai una notifica quando il tuo ordine sarà spedito.</p>
 
-            <button onClick={() => { navigate('/') }}>Torna alla HomePage</button>
+            <button className="btn btn-outline-light" onClick={() => { navigate('/') }}>Torna alla HomePage</button>
         </div>
     </>
 }
