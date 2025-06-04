a1e75204c5caa834d4d463c373aef3fc359ba8d3
giorgiameffe <giorgiameffe03@gmail.com>
2025-06-04 11:39:55 +0200

    feat: fix wishlist cards

--- bool-shop-react/src/components/CardWishList.jsx
+++ bool-shop-react/src/components/CardWishList.jsx
@@ -20,7 +20,7 @@ function CardWishList({ data }) {
     const discountPrice = hasDiscount ? (priceParsed - (priceParsed * discountAmountParsed / 100)).toFixed(2) : priceParsed.toFixed(2);
 
     return <>
-        <div className="gold-gradient-border-list mb-3 col-lg-6 col-sm-12 col-md-6 width-wish-cards">
+        {/* <div className="gold-gradient-border-list mb-3 col-lg-6 col-sm-12 col-md-6 width-wish-cards">
             <div className="card card-style mx-auto">
                 <div className="row g-0 align-items-center">
                     <div className="col-4 col-sm-4 col-md-4">
@@ -36,6 +36,57 @@ function CardWishList({ data }) {
                         </div>
                         <button className="btn-card text-white btn-sm mb-2 ms-3" onClick={() => addToCart(data)}>Aggiungi al carrello</button>
                     </div>
+                </div>
+            </div>
+        </div> */}
+
+        <div className="gold-gradient-border-list mb-3 col-lg-6 col-sm-12 col-md-6 width-wish-cards">
+            <div className="card card-style mx-auto h-100">
+                <div className="row g-0 align-items-stretch h-100">
+                    <div className="col-4 col-sm-4 col-md-4 d-flex flex-column align-items-center justify-content-center h-100">
+                        <FontAwesomeIcon
+                            icon={solidHeart}
+                            className={prefercolor(data) ? "text-danger" : ''}
+                            onClick={() => isPrefer(data)}
+                        />
+                        <Link to={`/detail/${slug}`}>
+                            <img
+                                src={`${VITE_BE_PATH}/img/${image}`}
+                                className="img-fluid rounded-start image-container h-auto"
+                                alt={name}
+                            />
+                        </Link>
+                    </div>
+                    <div className="col-8 col-sm-8 col-md-8 d-flex flex-column justify-content-between h-100 ">
+                        <div className="card-text-block p-2 ms-2">
+                            <h5 className="card-title fw-bold">{name}</h5>
+                            {hasDiscount ? (
+                                <div className="price-block">
+                                    <span className="text-decoration-line-through me-2 text-danger">
+                                        {priceParsed.toFixed(2)}€
+                                    </span>
+                                    <span className="fw-bold text-success">{discountPrice}€</span>
+                                    <span className="discount-price-list fw-bold ms-2">
+                                        - {discountAmountParsed}%
+                                    </span>
+                                </div>
+                            ) : (
+                                <div className="price-block">
+                                    <span>{priceParsed.toFixed(2)}€</span>
+                                </div>
+                            )}
+                        </div>
+
+                        <div className="mb-2 ms-3">
+                            <button
+                                className="btn-card text-white btn-sm"
+                                onClick={() => addToCart(data)}
+                            >
+                                Aggiungi al carrello
+                            </button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
